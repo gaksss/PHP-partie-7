06e01a4bf25a8203baef03054bd3125d1717c411
gaksss <clement.carlier2@gmail.com>
2024-12-05 15:11:26 +0100

    DONE: Exo2_3_4 partie 7

--- user.php
+++ user.php
@@ -1,8 +1,17 @@
 
 <!-- EXERCICE 1 PARTIE 7 -->
 <?php 
-$nom = $_GET["nom"];
-$prenom = $_GET["prenom"];
+// $nom = $_GET["nom"];
+// $prenom = $_GET["prenom"];
+// echo "Bienvenue " . $nom . " " . $prenom;
+
+
+
+// EXERCICE 2
+
+
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
 echo "Bienvenue " . $nom . " " . $prenom;
 
 ?>
