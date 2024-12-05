cf7b2a58cd9aaf99890e6ed73c17e7632eb3360c
gaksss <clement.carlier2@gmail.com>
2024-12-05 15:06:08 +0100

    DONE:Exo1 partie 7

---
+++ exo1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <?php
+
+    ?>
+    <form action="./user.php?nom=&prenom=" method="get">
+        <label for="nom">Nom: </label>
+        <input type="text" name="nom" id="nom" required maxlength="10">
+        <label for="prenom">Prénom: </label>
+        <input type="text" name="prenom" id="prenom" required maxlength="10">
+        <input type="submit" value="Envoyer">
+    </form>
+</body>
+</html>
---
+++ user.php
@@ -0,0 +1,8 @@
+
+<!-- EXERCICE 1 PARTIE 7 -->
+<?php 
+$nom = $_GET["nom"];
+$prenom = $_GET["prenom"];
+echo "Bienvenue " . $nom . " " . $prenom;
+
+?>
